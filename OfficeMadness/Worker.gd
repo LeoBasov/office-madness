18601a21696f09a20e8a5def4f3bf4043b921070
Leo Basov <l.basov@sparc-industries.com>
2021-04-11 21:16:34 +0200

    Added select stuff.

--- OfficeMadness/Worker.gd
+++ OfficeMadness/Worker.gd
@@ -1,1 +1,25 @@
 extends "res://actors/Actor.gd"
+
+var mouse_over : bool = false
+var selected : bool = false
+
+func _ready():
+	._ready()
+	$SelectIcon.play()
+	$SelectIcon.hide()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed and mouse_over:
+			selected = true
+			$SelectIcon.show()
+		elif event.button_index == BUTTON_LEFT and event.pressed and !mouse_over:
+			selected = false
+			$SelectIcon.hide()
+
+func _on_Area2D_mouse_entered():
+	mouse_over = true
+
+
+func _on_Area2D_mouse_exited():
+	mouse_over = false
