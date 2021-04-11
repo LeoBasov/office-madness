f81d158ea743d574e3654c71941525cbd8af688e
Leo Basov <l.basov@sparc-industries.com>
2021-04-11 22:02:52 +0200

    Modified blinking.

--- OfficeMadness/actors/worker/SelectIcon.gd
+++ OfficeMadness/actors/worker/SelectIcon.gd
@@ -2,14 +2,15 @@ extends AnimatedSprite
 
 var factor : float = 1.0
 var val : float = 0.0
+export var max_mod : float = 1.0
 export var min_mod : float = 0.5
-export var speed : float = 0.5
+export var blink_speed : float = 0.5
 
 func _process(delta):
-	val += delta*factor*speed
+	val += delta*factor*blink_speed
 	
-	if val > 1.0:
-		val = 1.0
+	if val > max_mod:
+		val = max_mod
 		factor = -1.0
 	elif val < min_mod:
 		val = min_mod
