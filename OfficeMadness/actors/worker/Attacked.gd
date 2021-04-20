f67de3dff50758c778b09df4c6f83267058027af
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-20 16:36:32 +0200

    Fixed attacked state

--- OfficeMadness/actors/worker/Attacked.gd
+++ OfficeMadness/actors/worker/Attacked.gd
@@ -4,6 +4,9 @@ var actor : AnimatedSprite
 var state_list =  load("res://actors/worker/StateList.gd").new()
 var return_state = null
 var speed : float = 100
+var old_position : Vector2
+var reaction : bool
+var animation : bool
 
 func initialize():
 	actor.get_node("Reaction").play()
@@ -12,13 +15,32 @@ func initialize():
 	actor.animation = "idle_down"
 	actor.play()
 	
+	old_position = actor.position
+	
+	$AnimationPlayer.play("level1")
+	
 	return_state = state_list.State.ATTACKED
 	
+	reaction = true
+	animation = true
+	
 func execute(delta):
+	if animation:
+		actor.position += old_position
+	
+	if !reaction and !animation:
+		return_state = state_list.State.IDLE
+	
 	return return_state
 
 func _on_Reaction_animation_finished():
 	actor.get_node("Reaction").stop()
 	actor.get_node("Reaction").hide()
 	
-	return_state = state_list.State.IDLE
+	reaction = false
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$AnimationPlayer.stop()
+	actor.position = old_position
+	animation = false
