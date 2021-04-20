24b075e64b196992d2591c856e0b18b3bf04b37c
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-20 15:12:44 +0200

    Removed Orb

--- OfficeMadness/Worker.gd
+++ OfficeMadness/Worker.gd
@@ -5,6 +5,7 @@ signal add_worker
 
 var mouse_over : bool = false
 var selected : bool = false
+var lifes : int = 3
 
 func _ready():
 	._ready()
@@ -12,7 +13,6 @@ func _ready():
 	$SelectIcon.hide()
 	
 	$Reaction.play()
-	$Reaction.hide()
 	
 	$Effect.object = self
 
@@ -48,3 +48,25 @@ func _on_object_selected(object):
 
 func _on_Ord_dead():
 	queue_free()
+
+func _on_Area2D_area_entered(area):
+	if area.get_owner().is_in_group("proffesor") and area.get_owner().get_node("FSM").current_state == area.get_owner().get_node("FSM").state_list.State.HUNT:
+		if lifes == 3:
+			$Reaction.play()
+			$Reaction.show()
+			lifes -= 1
+		elif lifes == 2:
+			$Reaction.play()
+			$Reaction.show()
+			lifes -= 1
+		elif lifes == 1:
+			$Reaction.play()
+			$Reaction.show()
+			lifes -= 1
+		elif lifes == 0:
+			queue_free()
+
+
+func _on_Reaction_animation_finished():
+	$Reaction.hide()
+	$Reaction.stop()
