42d2b9502b87a363347ae9850f8bfdc61c170752
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-21 09:52:17 +0200

    Updated stuff

--- GOAP/fsm/State.gd
+++ GOAP/fsm/State.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var fsm : Node2D
+var model : Node2D
 
 func initialize(new_fsm :  Node2D) -> void:
 	fsm = new_fsm
+	model = fsm.get_node("Model")
 
 func execute() -> void:
 	pass
