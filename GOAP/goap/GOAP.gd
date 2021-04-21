88b72e5265912b1ea7b5ca5d50a731f7bfda59b6
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-21 11:44:16 +0200

    Extended GOAP

--- GOAP/goap/GOAP.gd
+++ GOAP/goap/GOAP.gd
@@ -1,4 +1,12 @@
 extends Node
 
 var condition_state : Dictionary
-var fsm
+var fsm : Node
+var world_state : Node
+
+func _ready() -> void:
+	for action in $Actions.get_children():
+		action.initialize(self)
+
+func set_world_state(new_world_state : Node) -> void:
+	world_state = new_world_state
