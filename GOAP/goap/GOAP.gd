c27eb6ecbb28533769d421495991bae675cc90a9
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-21 13:06:34 +0200

    Implemented path building

--- GOAP/goap/GOAP.gd
+++ GOAP/goap/GOAP.gd
@@ -2,6 +2,7 @@ extends Node
 
 var fsm : Node
 var world_state : Node
+var condition_state : Dictionary
 
 class Leaf:
 	var condition_sate : Dictionary = {}
@@ -17,6 +18,10 @@ class Leaf:
 			self.condition_state[key] = action.effects[key]
 	
 		self.action = action
+		
+class ActionPath:
+	var valid : bool = false
+	var total_cost : float = 0.0
 
 func _ready() -> void:
 	for action in $Actions.get_children():
@@ -28,13 +33,13 @@ func initialize(new_world_state : Node) -> void:
 
 func _set_up() -> void:
 	# TO IMPLEMENT
-	# set up condition state
 	# set_ up FSM
+	# _set_up_condition_state
 	pass
 
-func build_tree(goal_key, goal_value) -> void:
+func _build_tree(goal_key, goal_value) -> void:
 	var availible_actions : Array = []
-	var root = Leaf.new(_set_up_condition_state())
+	var root = Leaf.new(condition_state)
 	
 	for action in $Actions.get_children():
 		action.reset()
@@ -43,10 +48,6 @@ func build_tree(goal_key, goal_value) -> void:
 			availible_actions.push_back(action)
 		
 	_build_leaf(root, availible_actions, goal_key, goal_value)
-
-func _set_up_condition_state() -> Dictionary:
-	# TO IMPLEMENT
-	return {}
 
 func _build_leaf(parent : Leaf, availible_actions : Array, goal_key, goal_value) -> void:
 	for action in availible_actions:
@@ -60,3 +61,20 @@ func _build_leaf(parent : Leaf, availible_actions : Array, goal_key, goal_value)
 				leaf.goal_reached = true
 			else:
 				_build_leaf(leaf, availible_actions, goal_key, goal_value)
+
+func _get_paths(leaf : Leaf, path : ActionPath, paths : Array) -> void:
+	for i in range(leaf.children.size()):
+		if i == 0:
+			path.total_cost += leaf.children[i].action.cost
+			path.valid = leaf.goal_reached
+			
+			_get_paths(leaf.children[i], path, paths)
+		else:
+			var new_path = ActionPath.new()
+			
+			paths.push_back(new_path)
+			
+			new_path.total_cost += leaf.children[i].action.cost
+			new_path.valid = leaf.goal_reached
+			
+			_get_paths(leaf.children[i], new_path, paths)
