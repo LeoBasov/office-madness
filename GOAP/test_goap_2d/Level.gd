bfc79fb54e527b55c0933d9a1d885896cb572e73
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-22 17:48:30 +0200

    Moved print text function

--- GOAP/test_goap_2d/Level.gd
+++ GOAP/test_goap_2d/Level.gd
@@ -6,6 +6,15 @@ var Fiel = preload("res://test_goap_2d/level/Field.tscn")
 func _ready() -> void:
 	$Miner.initialize(self)
 	
+	var text = "POSSIBLE ACTIONS\n"
+	text += "================\n"
+	
+	for action in $Miner/Actions.get_children():
+		text += action.name + "\n"
+		
+	$PossibleActions.text = text
+	
+# warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('ui_select'):
 		get_tree().paused = !get_tree().paused
