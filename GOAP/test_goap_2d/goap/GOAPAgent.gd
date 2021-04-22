8f5305a6b7ffcf8009ccf38f3df794d7284f774c
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-22 12:22:54 +0200

    Extended agent function

--- GOAP/test_goap_2d/goap/GOAPAgent.gd
+++ GOAP/test_goap_2d/goap/GOAPAgent.gd
@@ -24,7 +24,14 @@ func _set_up() -> void:
 func _action_canceled():
 	_plan(goal_key, goal_value)
 
-
 func _on_SleepTimer_timeout() -> void:
 	goal_key = "sleep"
 	goal_value = true
+
+
+func _on_Sleep_awake() -> void:
+	goal_key = "stored_iron"
+	goal_value = true
+	
+	_plan(goal_key, goal_value)
+	$SleepTimer.start()
