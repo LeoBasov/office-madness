d9ae1b33301644671e7ea00f990a1feede9b5112
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-22 17:22:43 +0200

    Added visualization of numbers

--- GOAP/test_goap_2d/Stack.gd
+++ GOAP/test_goap_2d/Stack.gd
@@ -1,3 +1,6 @@
 extends Sprite
 
 var axe_amount : int = 1
+
+func _process(delta: float) -> void:
+	$Label.text = String(axe_amount)
--- GOAP/test_goap_2d/Warehouse.gd
+++ GOAP/test_goap_2d/Warehouse.gd
@@ -1,3 +1,6 @@
 extends Sprite
 
 var ore_amount : int = 0
+
+func _process(delta: float) -> void:
+	$Label.text = String(ore_amount)
