d67cbc21b11bf17c96bcb2b12c686ec1a7a034ba
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2021-04-22 12:23:01 +0200

    Added stuff

--- GOAP/test_goap_2d/goap/Sleep.gd
+++ GOAP/test_goap_2d/goap/Sleep.gd
@@ -1,5 +1,7 @@
 extends "res://goap/Action.gd"
 
+signal awake
+
 var target = null
 var walk_state
 
@@ -15,16 +17,21 @@ func execute(delta : float) -> void:
 		if (target.position - goap.fsm.get_node("Actor").position).length() > walk_state.speed:
 			goap.fsm.pop_state()
 			goap.fsm.push_state(walk_state)
-		else:
-			effects["sleep"] = true
+		elif goap.condition_state["sleep"] == false:
+			goap.condition_state["sleep"] = true
 			goap.fsm.pop_state()
-			goap.pop_action()
+			$Timer.start()
 	else:
 		emit_signal("canceled")
 
 func _setpt_up_preconditions() -> void:
-	#preconditions["has_iron"] = false
-	pass
+	preconditions["has_iron"] = false
 
 func _setpt_up_effects() -> void:
 	effects["sleep"] = true
+
+
+func _on_Timer_timeout() -> void:
+	goap.condition_state["sleep"] = false
+	goap.pop_action()
+	emit_signal("awake")
