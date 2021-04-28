3eabe292c6eb1c2910cff64d4216402c567b5fb3
Leo Basov <l.basov@sparc-industries.com>
2021-04-28 14:35:39 +0200

    Added assests.

--- SmallOffice/test_2d/test_level_2d/Camera2D.gd
+++ SmallOffice/test_2d/test_level_2d/Camera2D.gd
@@ -1,11 +1,29 @@
 extends Camera2D
 
+var speed : float = 5.0
+var limit_low : Vector2
+var limit_high : Vector2
+
+func _ready():
+	limit_low.x = limit_left + 0.5*get_viewport().size.x
+	limit_low.y = limit_top + 0.5*get_viewport().size.y
+	
+	limit_high.x = limit_right - 0.5*get_viewport().size.x
+	limit_high.y = limit_bottom - 0.5*get_viewport().size.y
+	
+	print("limit_high ", limit_high)
+	print("limit_low ", limit_low)
+
 func _process(delta: float) -> void:
-	if Input.is_action_pressed('ui_right'):
-		offset += delta*Vector2(100, 0)
-	if Input.is_action_pressed('ui_left'):
-		offset -= delta*Vector2(100, 0)
-	if Input.is_action_pressed('ui_up'):
-		offset += delta*Vector2(0, 100)
-	if Input.is_action_pressed('ui_down'):
-		offset -= delta*Vector2(0, 100)
+	if Input.is_action_pressed('ui_right') and limit_high.x > get_camera_position().x:
+		offset_h += delta*speed
+		print(get_camera_position())
+	if Input.is_action_pressed('ui_left') and limit_low.x < get_camera_position().x:
+		offset_h -= delta*speed
+		print(get_camera_position())
+	if Input.is_action_pressed('ui_up') and limit_low.y < get_camera_position().y:
+		offset_v -= delta*speed
+		print(get_camera_position())
+	if Input.is_action_pressed('ui_down') and limit_high.y > get_camera_position().y:
+		offset_v += delta*speed
+		print(get_camera_position())
